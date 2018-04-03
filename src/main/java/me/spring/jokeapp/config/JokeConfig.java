80627033b15ca0e4ec9ef64138e32e58e94512af
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-03 18:25:51 +0300

    Added joke config and a bean of Chuck quotes

---
+++ src/main/java/me/spring/jokeapp/config/JokeConfig.java
@@ -0,0 +1,13 @@
+package me.spring.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JokeConfig {
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
