b9bd0301339913f9e583c86e3563e3c23f117006
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-03 17:37:18 +0300

    Added joke controller

---
+++ src/main/java/me/spring/jokeapp/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package me.spring.jokeapp.controllers;
+
+import me.spring.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "joke";
+    }
+}
